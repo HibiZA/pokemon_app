e2c17160e4ff5121e09f737cb2dcc30d03e87d7c
HibiZA <dylan.fuhri@gmail.com>
2020-08-03 21:08:36 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:pokemon_app/pages/homepage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+    );
+  }
+}
